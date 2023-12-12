08e1571a55672b463b56936af938f2f71aa5b40a
Mohd Abuzaid Ansari <mohdabuzaid15@gmail.com>
2023-12-13 01:05:53 +0530

    styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,47 @@
 import React from "react";
 import "./Navbar.css";
-
+import { FaPhone } from "react-icons/fa6";
+import { MdLocationPin } from "react-icons/md";
+import { FaFacebook } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
 export default function Navbar2() {
      return (
-          <nav className="navbar2" id="mainnav">
-               <ul className="navbar-links ">
-                    <li>
-                         Home
-                    </li>
-                    <li>
+          <div className="nav-cont">
+               <div className="prev-nav">
+                    <span className="loc">
+                         <MdLocationPin /> Dharavi</span>
+                    <span className="phone">
+                         <FaPhone />
+                         +91 7389265921</span>
+                    <div className="social-links">
+                         <a href="" className="fb"><FaFacebook /></a>
+                         <a href="" className="x"><FaTwitter /></a>
+                         <a href="" className="in"><FaLinkedinIn /></a>
+                         <a href="" className="insta"><RiInstagramFill /></a>
+                    </div>
+               </div>
+               <nav className="navbar2" id="mainnav">
+                    <ul className="navbar-links ">
+                         <li>
+                              Home
+                         </li>
+                         <li>
                               About
-                    </li>
-                    <li>
+                         </li>
+                         <li>
                               Class
-                    </li>
-                    <li>
+                         </li>
+                         <li>
                               Blog +
-                    </li>
-                    <li>
+                         </li>
+                         <li>
                               Contact
-                    </li>
-               </ul>
-          </nav>
+                         </li>
+                    </ul>
+               </nav>
+              
+          </div>
      );
 }
