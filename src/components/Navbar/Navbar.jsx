5c6ea0336ace67bd732f98870390afd2c16505fb
Mohd Abuzaid Ansari <mohdabuzaid15@gmail.com>
2023-12-13 00:12:36 +0530

    navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Navbar.css";
+
+export default function Navbar2() {
+     return (
+          <nav className="navbar2" id="mainnav">
+               <ul className="navbar-links ">
+                    <li>
+                         Home
+                    </li>
+                    <li>
+                              About
+                    </li>
+                    <li>
+                              Class
+                    </li>
+                    <li>
+                              Blog +
+                    </li>
+                    <li>
+                              Contact
+                    </li>
+               </ul>
+          </nav>
+     );
+}
