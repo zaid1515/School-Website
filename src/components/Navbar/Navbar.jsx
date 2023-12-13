5a42015d78ac8a45137c46650621041353e0bef9
Mohd Abuzaid Ansari <mohdabuzaid15@gmail.com>
2023-12-13 21:24:05 +0530

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,42 +6,50 @@ import { FaFacebook } from "react-icons/fa";
 import { RiInstagramFill } from "react-icons/ri";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
+import Mainlogo from "../../images/main-logo.png"
+
 export default function Navbar2() {
-     return (
-          <div className="nav-cont">
-               <div className="prev-nav">
-                    <span className="loc">
-                         <MdLocationPin /> Dharavi</span>
-                    <span className="phone">
-                         <FaPhone />
-                         +91 7389265921</span>
-                    <div className="social-links">
-                         <a href="" className="fb"><FaFacebook /></a>
-                         <a href="" className="x"><FaTwitter /></a>
-                         <a href="" className="in"><FaLinkedinIn /></a>
-                         <a href="" className="insta"><RiInstagramFill /></a>
-                    </div>
-               </div>
-               <nav className="navbar2" id="mainnav">
-                    <ul className="navbar-links ">
-                         <li>
-                              Home
-                         </li>
-                         <li>
-                              About
-                         </li>
-                         <li>
-                              Class
-                         </li>
-                         <li>
-                              Blog +
-                         </li>
-                         <li>
-                              Contact
-                         </li>
-                    </ul>
-               </nav>
-              
-          </div>
-     );
+  return (
+    <div className="nav-cont">
+      <div className="prev-nav">
+        <span className="loc">
+          <div className="loc-log">
+            <MdLocationPin />
+          </div>{" "}
+          Dharavi
+        </span>
+        <span className="phone">
+          <div className="ph-log">
+            <FaPhone />
+          </div>{" "}
+          +91 7389265921
+        </span>
+        <div className="social-links">
+          <a href="" className="fb nav-link" style={{ color: "white" }}>
+            <FaFacebook />
+          </a>
+          <a href="" className="x nav-link" style={{ color: "white" }}>
+            <FaTwitter />
+          </a>
+          <a href="" className="in nav-link" style={{ color: "white" }}>
+            <FaLinkedinIn />
+          </a>
+          <a href="" className="insta nav-link" style={{ color: "white" }}>
+            <RiInstagramFill />
+          </a>
+        </div>
+      </div>
+      <nav className="navbar2" id="mainnav">
+          <img src={Mainlogo} alt="logo" className="main-logo"/>
+        <ul className="navbar-list ">
+          <li>Home</li>
+          <li>About</li>
+          <li>Class</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ul>
+        <button className="enroll">Enroll Now</button>
+      </nav>
+    </div>
+  );
 }
