8385853be02efda1a63d6f458a53a389c608c4bd
Mohd Abuzaid Ansari <mohdabuzaid15@gmail.com>
2023-12-29 19:05:41 +0530

    carousel tr-cards hover effect styling

--- src/components/About3/About3.jsx
+++ src/components/About3/About3.jsx
@@ -1,30 +1,94 @@
-import React from 'react'
-import TrCard from '../TrCard/TrCard'
-import Tr1 from '../../images/tr1.webp'
-import Tr2 from '../../images/tr2.webp'
-import Tr3 from '../../images/tr3.webp'
-import './About3.css'
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import TrCard from '../TrCard/TrCard';
+import Tr1 from '../../images/tr1.webp';
+import Tr2 from '../../images/tr2.webp';
+import Tr3 from '../../images/tr3.webp';
+import './About3.css';
 
 function About3() {
-     return (
-          <div className="about3 tr-sect">
-               <div className="homeSec2-content sec">
-                    <h3 className="center-text">
-                         <div>
-                              Expert Teachers
-                         </div>
-                    </h3>
-                    <p className="homeSec2-para center-text">
-                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, excepturi omnis?  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, nihil.
-                    </p>
-               </div>
-               <div className="tr-details">
-                    <TrCard name="Mr. Anup Baloj" post="Sports Instructor" imgpath={Tr1}></TrCard>
-                    <TrCard name="Mrs. Rita Sharma" post="Sports Instructor" imgpath={Tr2}></TrCard>
-                    <TrCard name="Mr. Rohit Vashe" post="Sports Instructor" imgpath={Tr3}></TrCard>
-               </div>
-          </div>
-     )
+  const instructors = [
+    { name: "Mr. Anup Baloj", post: "Sports Instructor", imgpath: Tr1 },
+    { name: "Mrs. Rita Sharma", post: "Sports Instructor", imgpath: Tr2 },
+    { name: "Mr. Rohit Vashe", post: "Sports Instructor", imgpath: Tr3 },
+    { name: "Mr. Mohit Vashe", post: "Sports Instructor", imgpath: Tr3 },
+    { name: "Mr. Anup Baloj", post: "Sports Instructor", imgpath: Tr1 },
+    { name: "Mrs. Rita Sharma", post: "Sports Instructor", imgpath: Tr2 },
+    { name: "Mr. Rohit Vashe", post: "Sports Instructor", imgpath: Tr3 },
+    { name: "Mr. Mohit Vashe", post: "Sports Instructor", imgpath: Tr3 },
+  ];
+
+  const sliderSettings = {
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    centerMode: true,
+    centerPadding: '0',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+    prevArrow: <SamplePrevArrow />,
+    nextArrow: <SampleNextArrow />,
+  };
+
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: 'block', background: 'black' 
+      ,borderRadius:"100%"}}
+        onClick={onClick}
+      />
+    );
+  }
+
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: 'block', background: 'black', borderRadius:"100%"}}
+        onClick={onClick}
+      />
+    );
+  }
+
+  return (
+    <div className="about3 tr-sect">
+      <div className="home homeSec2-content sec">
+        <h3 className="center-text">
+          <div className='tr-sect-head'>Expert Teachers</div>
+        </h3>
+        <p className="homeSec2-para center-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, excepturi omnis? Lorem ipsum, dolor sit
+          amet consectetur adipisicing elit. Unde, nihil.
+        </p>
+      </div>
+      <Slider {...sliderSettings} className='tr-carousel'>
+        {instructors.map((tr, index) => (
+          <TrCard name={tr.name} post={tr.post} imgpath={tr.imgpath} key={index} />
+        ))}
+      </Slider>
+    </div>
+  );
 }
 
-export default About3
+export default About3;
--- src/components/TrCard/TrCard.jsx
+++ src/components/TrCard/TrCard.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
+import './TrCard.css'
+import { FaFacebook } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 function TrCard(props) {
   return (
     <div className="tr-card">
       <div className="tr-img">
-<img src={props.imgpath} alt="" />
+        <img src={props.imgpath} alt="" />
+        <div className="tr-caption">
+          <ul className="tr-social">
+            <li className="tr-link">
+              <Link to="https://www.facebook.com" style={{ color: "white" }}><FaFacebook /></Link>
+            </li>
+            <li className="tr-link"><Link to="https://www.linkedin.com" style={{ color: "white" }}><FaLinkedinIn /></Link></li>
+            <li className="tr-link">
+              <Link to="https://www.x.com" style={{ color: "white" }}>
+                <FaTwitter /></Link></li>
+            <li className="tr-link"><Link to="https://www.instagram.com" style={{ color: "white" }}><RiInstagramFill /></Link></li>
+          </ul>
+        </div>
       </div>
       <div className="single-tr-details">
-        <h2 className="tr-name">{props.name}
-        </h2>
-        <h3 className="tr-post">{props.post}</h3>
+        <h3 className="tr-name">{props.name}
+        </h3>
+        <h4 className="tr-post">{props.post}</h4>
       </div>
     </div>
   )
